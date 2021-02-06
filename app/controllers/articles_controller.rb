ebca8a2d4041652906dd413fa52226789378c9cd
Thomas Hillenbrand <thomashillenbrand16@gmail.com>
2021-02-06 12:57:44 -0500

    Removed byebug from articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
   def show
     # params passed in from the request
-    byebug
+    # byebug --> will open debug console and pause app
     @article = Article.find(params[:id]) # this is now an instance variable available in the view
   end
 
